be28bb7df1245e18432b6d219f632e0b7261b551
Vinicius Pavani <125597236+ViniciusPavani7@users.noreply.github.com>
2024-05-02 10:21:25 -0300

    Create atv1.sql

---
+++ atv1.sql
@@ -0,0 +1,48 @@
+    const mysql = require('mysql2');
+
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'phpmyadmin',
+      password: 'aluno',
+      database: 'CARROS'
+    });
+
+    connection.connect();
+
+    const queryString = "SELECT * FROM CARROS WHERE COR = 'verde'";
+
+    connection.query(queryString, (error, results, fields) => {
+      if (error) throw error;
+      console.log('Resultado da consulta:');
+      console.log(results);
+    });
+
+    connection.end
+
+
+
+/////////////////////////////////////////////////////////////////////
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'phpmyadmin',
+  password: 'aluno',
+  database: 'CARROS'
+});
+
+connection.connect();
+
+const createProcedureQuery = `
+  CREATE PROCEDURE ConsultarCarrosVerdes()
+  BEGIN
+    SELECT * FROM CARROS WHERE COR = 'verde';
+  END`;
+
+connection.query(createProcedureQuery, (error, results, fields) => {
+  if (error) throw error;
+  console.log('Stored procedure criada com sucesso!');
+});
+
+connection.end();
