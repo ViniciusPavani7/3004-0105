80f26d1298f3d29143ba14dedff7e53ed3b14c20
Vinicius Pavani <125597236+ViniciusPavani7@users.noreply.github.com>
2024-05-02 10:23:44 -0300

    Update atv2.sql

--- atv2.sql
+++ atv2.sql
@@ -1,1 +1,48 @@
+    const mysql = require('mysql2');
 
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'phpmyadmin',
+      password: 'aluno',
+      database: 'CARROS'
+    });
+
+    connection.connect();
+
+    const queryString = "SELECT * FROM CARROS WHERE CÓDIGO = 3";
+
+    connection.query(queryString, (error, results, fields) => {
+      if (error) throw error;
+      console.log('Resultado da consulta:');
+      console.log(results);
+    });
+
+    connection.end
+
+
+
+/////////////////////////////////////////////////////////////////////
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'phpmyadmin',
+  password: 'aluno',
+  database: 'CARROS'
+});
+
+connection.connect();
+
+const createProcedureQuery = `
+  CREATE PROCEDURE ConsultarCarro3()
+  BEGIN
+    SELECT * FROM CARROS WHERE CÓDIGO = 3;
+  END`;
+
+connection.query(createProcedureQuery, (error, results, fields) => {
+  if (error) throw error;
+  console.log('Stored procedure criada com sucesso!');
+});
+
+connection.end();
