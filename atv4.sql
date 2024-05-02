c6b9f1a7c9fec32876ebdd798402f6e23f55479e
Vinicius Pavani <125597236+ViniciusPavani7@users.noreply.github.com>
2024-05-02 13:55:54 -0300

    Update atv4.sql

--- atv4.sql
+++ atv4.sql
@@ -25,17 +25,28 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'phpmyadmin',
   password: 'aluno',
-  database: 'ATIVIDADE3004'
+  database: 'CARROS'
 });
 
 connection.connect();
 
-// Chamada da stored procedure
-connection.query('CALL ConsultarCarrosDisponiveis()', (error, results, fields) => {
+// Query para criar a stored procedure
+const createProcedureQuery = `
+  DELIMITER //
+  CREATE PROCEDURE ConsultarCarrosDisponiveis()
+  BEGIN
+    SELECT * FROM CARROS 
+    WHERE ESTÁ_DISPONIVEL = 'SIM' 
+    AND CÓDIGO NOT IN (SELECT CODCARRO FROM LOCAÇÕES);
+  END //
+  DELIMITER ;
+`;
+
+// Executa a query para criar a stored procedure
+connection.query(createProcedureQuery, (error, results, fields) => {
   if (error) throw error;
-  console.log('Resultado da consulta:');
-  console.log(results[0]);
+  console.log('Stored procedure criada com sucesso!');
+  connection.end();
 });
 
-connection.end();
 
