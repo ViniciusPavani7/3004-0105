5ecdf80a514716abde9192713baa6a5497193067
Vinicius Pavani <125597236+ViniciusPavani7@users.noreply.github.com>
2024-05-02 13:53:57 -0300

    Update atv3.sql

--- atv3.sql
+++ atv3.sql
@@ -33,11 +33,32 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-// Chamada da stored procedure
-connection.query('CALL ConsultarLocacoesCombustivel()', (error, results, fields) => {
+const queryString = "SELECT LOCAÇÕES.*, CLIENTES.NOME_CLIENTE, CLIENTES.TELEFONE FROM LOCAÇÕES JOIN CARROS ON LOCAÇÕES.CODCARRO = CARROS.CÓDIGO JOIN CLIENTES ON LOCAÇÕES.CODCLIENTE = CLIENTES.CÓDIGO WHERE CARROS.COMBUSTÍVEL IN ('Gasolina', 'Etanol')";
+
+connection.query(queryString, (error, results, fields) => {
   if (error) throw error;
   console.log('Resultado da consulta:');
-  console.log(results[0]); //
+  console.log(results);
+
+  const createProcedureQuery = `
+    DELIMITER //
+    CREATE PROCEDURE ConsultarLocacoesClientes()
+    BEGIN
+      SELECT LOCAÇÕES.*, CLIENTES.NOME_CLIENTE, CLIENTES.TELEFONE 
+      FROM LOCAÇÕES 
+      JOIN CARROS ON LOCAÇÕES.CODCARRO = CARROS.CÓDIGO 
+      JOIN CLIENTES ON LOCAÇÕES.CODCLIENTE = CLIENTES.CÓDIGO 
+      WHERE CARROS.COMBUSTÍVEL IN ('Gasolina', 'Etanol');
+    END //
+    DELIMITER ;
+  `;
+
+  connection.query(createProcedureQuery, (error, results, fields) => {
+    if (error) throw error;
+    console.log('Stored procedure criada com sucesso!');
+
+    connection.end(); // Encerrar a conexão após criar a stored procedure
+  });
 });
 
-connection.end();
+
